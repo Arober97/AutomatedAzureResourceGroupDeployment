4b32b6a9d9815f7910e25eda3ba94baab28c31b5
Adam Roberts <59274771+TechEnchante@users.noreply.github.com>
2023-06-17 20:34:48 -0400

    Update Azure_CLI_Deployment_Script.sh
    Changes to match those made in the PowerShell script. 
    
    Tested and working.

--- Azure_CLI_Deployment_Script.sh
+++ Azure_CLI_Deployment_Script.sh
@@ -10,14 +10,12 @@ function Log-Operation {
 function Create-ResourceGroup {
     resourceGroup="$1"
     location="$2"
-    try {
-        Log-Operation "Creating Resource Group with name $resourceGroup and location $location"
-        az group create --name $resourceGroup --location $location
-    }
-    catch {
-        Log-Operation "Error creating Resource Group: $?"
+    Log-Operation "Creating Resource Group with name $resourceGroup and location $location"
+    az group create --name $resourceGroup --location $location
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error creating Resource Group"
         exit 1
-    }
+    fi
 }
 
 # Function to create Virtual Machine
@@ -25,14 +23,60 @@ function Create-VM {
     resourceGroup="$1"
     location="$2"
     vmName="$3"
-    try {
-        Log-Operation "Creating VM named $vmName in resource group $resourceGroup and location $location"
-        az vm create --resource-group $resourceGroup --name $vmName --image UbuntuLTS --admin-username azureuser --generate-ssh-keys
-    }
-    catch {
-        Log-Operation "Error creating VM: $?"
+    Log-Operation "Creating VM named $vmName in resource group $resourceGroup and location $location"
+    az vm create --resource-group $resourceGroup --name $vmName --image UbuntuLTS --admin-username azureuser --generate-ssh-keys
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error creating VM"
         exit 1
-    }
+    fi
+}
+
+# Function to start Virtual Machine
+function Start-VM {
+    resourceGroup="$1"
+    vmName="$2"
+    Log-Operation "Starting VM named $vmName in resource group $resourceGroup"
+    az vm start --resource-group $resourceGroup --name $vmName
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error starting VM"
+        exit 1
+    fi
+}
+
+# Function to stop Virtual Machine
+function Stop-VM {
+    resourceGroup="$1"
+    vmName="$2"
+    Log-Operation "Stopping VM named $vmName in resource group $resourceGroup"
+    az vm stop --resource-group $resourceGroup --name $vmName
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error stopping VM"
+        exit 1
+    fi
+}
+
+# Function to restart Virtual Machine
+function Restart-VM {
+    resourceGroup="$1"
+    vmName="$2"
+    Log-Operation "Restarting VM named $vmName in resource group $resourceGroup"
+    az vm restart --resource-group $resourceGroup --name $vmName
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error restarting VM"
+        exit 1
+    fi
+}
+
+# Function to delete Virtual Machine
+function Delete-VM {
+    resourceGroup="$1"
+    vmName="$2"
+    Log-Operation "Deleting VM named $vmName in resource group $resourceGroup"
+    az vm delete --resource-group $resourceGroup --name $vmName --yes
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error deleting VM"
+        exit 1
+    fi
 }
 
 # Function to create Storage Account
@@ -40,14 +84,12 @@ function Create-Storage {
     resourceGroup="$1"
     location="$2"
     storageAccountName="$3"
-    try {
-        Log-Operation "Creating Storage Account with name $storageAccountName in resource group $resourceGroup and location $location"
-        az storage account create --name $storageAccountName --resource-group $resourceGroup --location $location --sku Standard_LRS
-    }
-    catch {
-        Log-Operation "Error creating Storage Account: $?"
+    Log-Operation "Creating Storage Account with name $storageAccountName in resource group $resourceGroup and location $location"
+    az storage account create --name $storageAccountName --resource-group $resourceGroup --location $location --sku Standard_LRS
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error creating Storage Account"
         exit 1
-    }
+    fi
 }
 
 # Function to create VNet
@@ -55,40 +97,23 @@ function Create-VNet {
     resourceGroup="$1"
     location="$2"
     vNetName="$3"
-    try {
-        Log-Operation "Creating VNet named $vNetName in resource group $resourceGroup and location $location"
-        az network vnet create --resource-group $resourceGroup --name $vNetName --subnet-name MySubnet
-    }
-    catch {
-        Log-Operation "Error creating VNet: $?"
+    Log-Operation "Creating VNet named $vNetName in resource group $resourceGroup and location $location"
+    az network vnet create --resource-group $resourceGroup --name $vNetName --subnet-name MySubnet
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error creating VNet"
         exit 1
-    }
+    fi
 }
 
 # Function to delete Resource Group
 function Delete-ResourceGroup {
     resourceGroup="$1"
-    try {
-        Log-Operation "Deleting Resource Group with name $resourceGroup"
-        az group delete --name $resourceGroup --yes --no-wait
-    }
-    catch {
-        Log-Operation "Error deleting Resource Group: $?"
+    Log-Operation "Deleting Resource Group with name $resourceGroup"
+    az group delete --name $resourceGroup --yes --no-wait
+    if [[ $? -ne 0 ]]; then
+        Log-Operation "Error deleting Resource Group"
         exit 1
-    }
-}
-
-# Function to update resources in the Resource Group
-function Update-Resources {
-    resourceGroup="$1"
-    try {
-        Log-Operation "Updating resources in Resource Group $resourceGroup"
-        # Add your update logic here
-    }
-    catch {
-        Log-Operation "Error updating resources: $?"
-        exit 1
-    }
+    fi
 }
 
 # Function to prompt for user input and create resources
@@ -110,22 +135,28 @@ function Prompt-And-Create-Resources {
     fi
 }
 
+# Function to prompt for user input and update resources
+function Prompt-And-Update-Resources {
+    read -p "Enter the Resource Group Name: " resourceGroup
+    read -p "Enter the VM Name: " vmName
+    read -p "Do you want to (s)tart, sto(p), (r)estart, or (d)elete the VM? " vmAction
+    case "$vmAction" in
+        's') Start-VM $resourceGroup $vmName ;;
+        'p') Stop-VM $resourceGroup $vmName ;;
+        'r') Restart-VM $resourceGroup $vmName ;;
+        'd') Delete-VM $resourceGroup $vmName ;;
+    esac
+}
+
 # Main function to execute the script
 function Main {
-    try {
-        az login
-        Prompt-And-Create-Resources
-        # Uncomment the following line if you want to update the resources
-        # Update-Resources $resourceGroup
-        # Uncomment the following line if you want to delete the Resource Group at the end of the script
-        # Delete-ResourceGroup $resourceGroup
-    }
-    catch {
-        Log-Operation "An error occurred: $?"
-        exit 1
-    }
+    az login
+    read -p "Do you want to (c)reate or (u)pdate resources? " action
+    case "$action" in
+        'c') Prompt-And-Create-Resources ;;
+        'u') Prompt-And-Update-Resources ;;
+    esac
 }
 
 # Call the main function to start the script
 Main
-
